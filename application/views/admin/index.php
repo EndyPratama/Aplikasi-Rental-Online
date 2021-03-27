cd64c14c1298c542e6733f1d11b0f795a987eb96
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-03-28 05:57:27 +0700

    template

---
+++ application/views/admin/index.php
@@ -0,0 +1,17 @@
+<!-- MAIN -->
+<div class="col-8">
+
+    <h1>
+        Test Index
+    </h1>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+    </ul>
+
+</div>
+</div>
