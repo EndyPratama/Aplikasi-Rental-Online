247b91eac19493e99c4e1000b530d2b99588cde9
Justitian <57588923+Justitian@users.noreply.github.com>
2021-04-13 12:21:03 +0700

    Update Booking

--- application/models/M_Kendaraan.php
+++ application/models/M_Kendaraan.php
@@ -46,6 +46,7 @@ class M_Kendaraan extends CI_Model
     {
         $this->db->select("*");
         $this->db->from("transaksi");
+
         // $this->db->where('pesan.user_id=akun.id');
         $query = $this->db->get();
         return $query->result();
@@ -53,36 +54,37 @@ class M_Kendaraan extends CI_Model
     public function getBooking()
     {
 
-        $this->db->select("*");
+        $this->db->select("id, peminjam, alamat, kendaraan, durasi, booking.harga");
         $this->db->from("booking");
         $this->db->join('kendaraan', 'booking.kendaraan = kendaraan.nama', 'left');
+        $this->db->where('action = 0');
         $query = $this->db->get();
         return $query->result();
     }
-    public function getIdKendaraan($id_user)
+    public function getIdKendaraan($id)
     {
 
         $this->db->select("id_kendaraan");
         $this->db->from("kendaraan");
         $this->db->join('booking', 'kendaraan.nama = booking.kendaraan', 'right');
-        $this->db->where('id_user', $id_user);
+        $this->db->where('id', $id);
         $query = $this->db->get();
         return $query->row()->id_kendaraan;
     }
-    public function getHarga($id_user)
+    public function getHarga($id)
     {
         $this->db->select("harga");
         $this->db->from("booking");
-        $this->db->where("id_user", $id_user);
+        $this->db->where("id", $id);
         // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
         $query = $this->db->get();
         return $query->row()->harga;
     }
-    public function getIdUser($id_user)
+    public function getIdUser($id)
     {
         $this->db->select("id_user");
         $this->db->from("booking");
-        $this->db->where("id_user", $id_user);
+        $this->db->where("id", $id);
         // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
         $query = $this->db->get();
         return $query->row()->id_user;
