6e538446c4b40cae9e48e29ae31f46ef8a1a3fe5
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-04-26 18:39:44 +0700

    harap folder diganti jadi vendor

---
+++ vendor_/public/js/order.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    console.log("Haii");
+    // var akun = $('#walletaddress').val();
+    // console.log("Wallet : " + akun);
+    // $.ajax({
+    //     url: 'https://api.etherscan.io/api',
+    //     type: 'get',
+    //     dataType: 'json',
+    //     data: {
+    //         'module': 'account',
+    //         'action': 'balance',
+    //         'address': akun,
+    //         'tag': 'latest',
+    //         'apikey': 'APIKEY'
+    //     },
+    //     success: function(result) {
+    //         console.log(result);
+    //         balance = result.result;
+    //         document.getElementById("balance").innerHTML = balance + " ETH";
+    //     }
+    // });
+});
