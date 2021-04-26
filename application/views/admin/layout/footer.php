a3024cb5961fc0cfe31c39cf0722c272a7eb0f66
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-04-26 18:29:54 +0700

    tambah script JS

--- application/views/admin/layout/footer.php
+++ application/views/admin/layout/footer.php
@@ -1,3 +1,4 @@
+</div>
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
@@ -9,6 +10,18 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
+<script>
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+    let searchBTN = document.querySelector(".bx-search");
+
+    btn.onclick = function() {
+        sidebar.classList.toggle("active");
+    }
+    searchBTN.onclick = function() {
+        sidebar.classList.toggle("active");
+    }
+</script>
 </body>
 
 </html>
