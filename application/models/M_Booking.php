a5cc0244e2074fa1458fd6a2068f764e56a1e199
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-04-27 13:39:21 +0700

    pembuatan booking

--- application/models/M_Booking.php
+++ application/models/M_Booking.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class M_Transaksi extends CI_Model
+class M_Booking extends CI_Model
 {
     // protected $komik = 'komik';
     // protected $member = 'member';
@@ -23,5 +23,33 @@ class M_Transaksi extends CI_Model
         // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
         $query = $this->db->get();
         return $query->result();
+    }
+    public function getIdKendaraan($id)
+    {
+
+        $this->db->select("id_kendaraan");
+        $this->db->from("kendaraan");
+        $this->db->join('booking', 'kendaraan.nama = booking.kendaraan', 'right');
+        $this->db->where('id', $id);
+        $query = $this->db->get();
+        return $query->row()->id_kendaraan;
+    }
+    public function getHarga($id)
+    {
+        $this->db->select("harga");
+        $this->db->from("booking");
+        $this->db->where("id", $id);
+        // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
+        $query = $this->db->get();
+        return $query->row()->harga;
+    }
+    public function getIdUser($id)
+    {
+        $this->db->select("id_user");
+        $this->db->from("booking");
+        $this->db->where("id", $id);
+        // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
+        $query = $this->db->get();
+        return $query->row()->id_user;
     }
 }
