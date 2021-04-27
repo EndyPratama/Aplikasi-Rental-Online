5843994f2c6aabe2df1011eb64bffb6d326566d7
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-04-27 13:48:19 +0700

    perubahan function

--- application/models/M_Kendaraan.php
+++ application/models/M_Kendaraan.php
@@ -61,34 +61,7 @@ class M_Kendaraan extends CI_Model
         $query = $this->db->get();
         return $query->result();
     }
-    public function getIdKendaraan($id)
-    {
 
-        $this->db->select("id_kendaraan");
-        $this->db->from("kendaraan");
-        $this->db->join('booking', 'kendaraan.nama = booking.kendaraan', 'right');
-        $this->db->where('id', $id);
-        $query = $this->db->get();
-        return $query->row()->id_kendaraan;
-    }
-    public function getHarga($id)
-    {
-        $this->db->select("harga");
-        $this->db->from("booking");
-        $this->db->where("id", $id);
-        // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
-        $query = $this->db->get();
-        return $query->row()->harga;
-    }
-    public function getIdUser($id)
-    {
-        $this->db->select("id_user");
-        $this->db->from("booking");
-        $this->db->where("id", $id);
-        // select * from pesan JOIN akun on pesan.user_id=akun.id join jawaban on pesan.id_pesan = jawaban.id_pesan where pesan.user_id='6' 
-        $query = $this->db->get();
-        return $query->row()->id_user;
-    }
     public function getIklan($merk)
     {
         $this->db->select("*");
