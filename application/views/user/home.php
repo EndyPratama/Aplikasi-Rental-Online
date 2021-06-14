41980036ddeb23a4af25b94d5b4d633e33b78a8e
aghilsah <aghilsyahputro@gmail.com>
2021-06-14 17:40:50 +0700

    fixing homepage

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -1,3 +1,11 @@
+<div class="container-fluid jumbotron">
+    <h2>
+    RENO <br>
+    </h2>
+    RENO adalah penyedia jasa rental kendaraan online yang menggunakan website
+    sebagai platform transaksi. <br>
+    RENO mengedepankan kecepatan, kemudahan dan kepercayaan pengguna terhadap layanan yang digunakan
+</div>
 <div class="container-fluid carousel-contain">
     <div class="container bordered">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -10,19 +18,19 @@
             </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="<?= base_url('/vendor/public/img/jpg1.jpg')?>" class="d-block w-100" alt="...">
+            <img src="<?= base_url('/vendor/public/img/jpg1.jpg')?>" class="tengah" alt="...">
             </div>
             <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg7.jpg')?>" class="d-block w-100" alt="...">
+            <img src="<?= base_url('/vendor/public/img/jpg7.jpg')?>" class="tengah" alt="...">
             </div>
             <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg3.jpg')?>" class="d-block w-100" alt="...">
+            <img src="<?= base_url('/vendor/public/img/jpg3.jpg')?>" class="tengah" alt="...">
             </div>
             <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg4.jpg')?>" class="d-block w-100" alt="...">
+            <img src="<?= base_url('/vendor/public/img/jpg4.jpg')?>" class="tengah" alt="...">
             </div>
             <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg6.jpg')?>" class="d-block w-100" alt="...">
+            <img src="<?= base_url('/vendor/public/img/jpg6.jpg')?>" class="tengah" alt="...">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -35,17 +43,4 @@
         </a>
         </div>
     </div>
-    
-
 </div>
-
-<div class="desc py-5">
-    <h2>
-    RENO <br>
-    </h2>
-    RENO adalah penyedia jasa rental kendaraan online yang menggunakan website
-    sebagai platform transaksi. <br>
-    RENO mengedepankan kecepatan, kemudahan dan kepercayaan pengguna terhadap layanan yang digunakan
-</div>
-
-
