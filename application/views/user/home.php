8b5970d82830a771a28f90d870229602e7da5d47
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-06-15 10:07:35 +0700

    perbaikan carausel

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -8,44 +8,42 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
             </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="<?= base_url('/vendor/public/img/jpg1.jpg')?>" class="d-block w-100" alt="...">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?= base_url('/vendor/public/img/jpg1.jpg') ?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('/vendor/public/img/jpg7.jpg') ?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('/vendor/public/img/jpg3.jpg') ?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('/vendor/public/img/jpg4.jpg') ?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= base_url('/vendor/public/img/jpg6.jpg') ?>" class="d-block w-100" alt="...">
+                </div>
             </div>
-            <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg7.jpg')?>" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg3.jpg')?>" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg4.jpg')?>" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="<?= base_url('/vendor/public/img/jpg6.jpg')?>" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
-    
+
 
 </div>
 
 <div class="desc py-5">
     <h2>
-    RENO <br>
+        RENO <br>
     </h2>
     RENO adalah penyedia jasa rental kendaraan online yang menggunakan website
     sebagai platform transaksi. <br>
     RENO mengedepankan kecepatan, kemudahan dan kepercayaan pengguna terhadap layanan yang digunakan
 </div>
-
-
