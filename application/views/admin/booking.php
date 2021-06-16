8d310bd0f974d83a96ba3d6c461d76a83fa28ded
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-06-16 15:39:57 +0700

    booking

--- application/views/admin/booking.php
+++ application/views/admin/booking.php
@@ -40,38 +40,8 @@
                                 <td><?= $b->durasi; ?> Hari</td>
                                 <td>Rp <?= $total; ?></td>
                                 <td class="align-middle">
-                                    <a class="btn btn-success" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye"> Lihat</i></a>
-
+                                    <a href="<?= base_url('admin/booking/terima/' . $b->id); ?>" class="btn btn-success"><i class="fa fa-check"> Terima</i></a>
                                     <a href="<?= base_url('admin/booking/tolak/' . $b->id); ?>" class="btn btn-danger"><i class="fa fa-remove"> Tolak</i></a>
-
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Bukti Pembayaran</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="gambar" style="width:100%;">
-                                                        <?php if ($b->bukti_transaksi != NULL) { ?>
-                                                            <img src="<?= base_url('vendor/public/img/' . $b->bukti_transaksi) ?>" alt="transaksi" style="width:100%;">
-                                                        <?php } else { ?>
-                                                            <p>Belum melakukan upload bukti pembayaran...</p>
-                                                        <?php } ?>
-                                                    </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <?php if ($b->bukti_transaksi != NULL) { ?>
-                                                        <a href="<?= base_url('admin/booking/terima/' . $b->id); ?>" class="btn btn-success"><i class="fa fa-check"> Terima</i></a>
-                                                    <?php } ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </td>
                             </tr>
                         <?php
